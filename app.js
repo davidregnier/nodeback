fa57de24f12feadd385db1f8f337afe3c1acc8a8
EpicMaDneSss <regnier.david.pro@gmail.com>
2023-04-11 14:47:17 +0200

    post et delete

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const myConnection = require('express-myconnection');
-const port = 3000;
+const bodyParser = require('body-parser');
+const port = 8080;
 const optionBDD = {
   host: 'localhost',
   user: 'root',
@@ -11,6 +12,7 @@ const optionBDD = {
   database: 'todolist'
 }
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(myConnection(mysql, optionBDD, 'pool'));
 app.use(express.static('public')); //definition des resources static
 app.set('views', './IHM'); //definition du chemin de mes views
@@ -32,6 +34,42 @@ app.get('/', (req, res)=>{
   })
 });
 
+app.post('/ajouter-tache', (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  req.getConnection((error, connection) => {
+    if (error) {
+      console.error(error);
+    } else {
+      connection.query('INSERT INTO liste SET ?', { title, description }, (error, result) => {
+        if (error) {
+          console.error(error);
+        } else {
+          res.redirect('/');
+        }
+      });
+    }
+  });
+});
+
+app.post('/supprimer-tache/:id', (req, res) => {
+  const id = req.params.id;
+  req.getConnection((error, connection) => {
+    if (error) {
+      console.error(error);
+    } else {
+      connection.query('DELETE FROM liste WHERE id = ?', [id], (error, result) => {
+        if (error) {
+          console.error(error);
+        } else {
+          res.redirect('/');
+        }
+      });
+    }
+  });
+});
+
+
 app.get('/a-propos', (req, res)=>{
   res.status(200).render('apropos')
 });
@@ -43,3 +81,5 @@ app.use((req, res)=>{
 app.listen(port, ()=>{
   console.log("Server listening on port " + port);
 });
+
+
